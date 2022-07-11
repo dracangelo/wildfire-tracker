def2173036344942386661476f9d308877fb7f1f
dracangelo <kelvinmbuguaw@gmail.com>
2022-07-11 13:21:29 +0300

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import spinner from './spinner.gif'
+
+export const Loader = () => {
+  return (
+    <div className="loader">
+        <img src= {spinner} alt="Loading..."/>
+        <h1>fetching data</h1>
+    </div>
+  )
+}
+
+export default Loader
