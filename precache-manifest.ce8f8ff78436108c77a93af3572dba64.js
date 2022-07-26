987f9c28559b7525b00e2576bab9badd30a27f7e
dracangelo <kelvinmbuguaw@gmail.com>
2022-07-26 21:35:16 +0300

    Updates

---
+++ precache-manifest.ce8f8ff78436108c77a93af3572dba64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6333bf305cf6adf344b8",
+    "url": "/wildfire-tracker/static/css/main.5c141934.chunk.css"
+  },
+  {
+    "revision": "6333bf305cf6adf344b8",
+    "url": "/wildfire-tracker/static/js/main.6333bf30.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/wildfire-tracker/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "a0be69f20c8e3420d8bf",
+    "url": "/wildfire-tracker/static/js/2.a0be69f2.chunk.js"
+  },
+  {
+    "revision": "1f4255b0829f36d8f4f1",
+    "url": "/wildfire-tracker/static/js/runtime~main.1f4255b0.js"
+  },
+  {
+    "revision": "ae2ed7ed6a1606a2a59e470c7e9e6a37",
+    "url": "/wildfire-tracker/static/media/spinner.ae2ed7ed.gif"
+  },
+  {
+    "revision": "08479406cd7e7c11c5396a1cf213c61f",
+    "url": "/wildfire-tracker/index.html"
+  }
+];
