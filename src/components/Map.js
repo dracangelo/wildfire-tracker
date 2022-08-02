ea29924126c9b95bbcbd3ad5c6aa9f1fa25afb76
dracangelo <kelvinmbuguaw@gmail.com>
2022-08-02 13:29:29 +0300

    added a volcano tracker

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,14 +3,23 @@ import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 import LocationInfoBox from './LocationInfoBox'
+import VolcanoMarker from './VolcanoMarker'
 
 // define constants
+const VOLCANO = 12;
 const NATURAL_EVENT_WILDFIRE = 8;
 
 const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
     const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === VOLCANO) {
+            return <VolcanoMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
+        return null
+    })
+
+    const marker = eventData.map((ev, index) => {
         if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
             return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
@@ -20,11 +29,12 @@ const Map = ({ eventData, center, zoom }) => {
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyB-RIMmc8a7ybn_Jrj0OBTtIhVq1-VGf-k' }}
+                bootstrapURLKeys={{ key: '' }}
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
                 {markers}
+                {marker}
             </GoogleMapReact>
             {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
---
+++ src/components/VolcanoMarker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+import volcanoIcon from '@iconify/icons-wi/volcano';
+
+const VolcanoMarker = ({ lat, lng, onClick }) => {
+    return (
+        <div className="location-marker" onClick={onClick}>
+            <Icon icon={volcanoIcon} className="location-icon" />
+        </div>
+    )
+}
+
+export default VolcanoMarker
